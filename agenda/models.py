bceb26b476a95fbd63bccb28d7ad668a74bb8f72
Nycollas Henich <nhhenich@gmail.com>
2024-06-21 15:05:51 -0300

    models, contato

--- agenda/models.py
+++ agenda/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# criando models que ficaram no banco de dados
+# unique=True, usado para garantir que não terá dois valores iguais
+
+class Contato(models.Model):
+    nome = models.CharField('Nome', max_length=100)
+    telefone = models.CharField('Telefone', max_length=15)
+    email = models.EmailField(unique=True)
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.nome
