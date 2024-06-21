e5b14b2689c3334f4198c26100520347f878e101
Nycollas Henich <nhhenich@gmail.com>
2024-06-21 14:59:11 -0300

    criação da lista de contatos

--- agenda/urls.py
+++ agenda/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from agenda.views import index
+from agenda.views import index, listadecontatos
 
 urlpatterns = [
     path('', index, name='index'),
+    path('listadecontatos/', listadecontatos, name='listadecontatos')
 ]
--- agenda/views.py
+++ agenda/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def listadecontatos(request):
+    return render(request, 'lista_de_contatos.html')
