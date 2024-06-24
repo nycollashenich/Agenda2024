5aa8065e4cbbbba0da5de9db10c6d9885936863d
Nycollas Henich <nhhenich@gmail.com>
2024-06-23 23:35:19 -0300

    add imagem no model

--- agenda/models.py
+++ agenda/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+from stdimage.models import StdImageField
 
 # criando models que ficaram no banco de dados
 # unique=True, usado para garantir que não terá dois valores iguais
 
 class Contato(models.Model):
-    primeiro_nome = models.CharField('Primeiro nome', max_length=100)
-    segundo_nome = models.CharField('Segundo nome', max_length=100)
-    telefone = models.CharField('Telefone', max_length=15)
-    email = models.EmailField(unique=True)
+    primeiro_nome = models.CharField('Primeiro nome', max_length=100, blank=False)
+    segundo_nome = models.CharField('Segundo nome', max_length=100, blank=True)
+    telefone = models.CharField('Telefone', max_length=15, blank=False)
+    email = models.EmailField(unique=True, blank=False)
     criado_em = models.DateTimeField(auto_now_add=True)
+    imagem = StdImageField('Imagem', upload_to='rostos_contatos', variations={'thumb': (124, 124)}, blank=True)
 
     def __str__(self) -> str:
         return self.primeiro_nome
